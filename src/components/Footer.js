d64d6954d5ccf5dc80fa2fa0452b4b5d16f83943
Juliano Boese <juliano.boese@gmail.com>
2022-01-25 20:58:03 -0300

    Adiciona componente Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LogoTrybe from '../images/logo-trybe.png';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <img src={ LogoTrybe } alt="Logo da Trybe" />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
